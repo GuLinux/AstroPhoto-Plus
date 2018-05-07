2bd74bdb2b97d8860fa871fe958e940142d452e9
Marco Gulino <marco.gulino@gmail.com>
2018-05-07 19:59:49 +0100

    Draft for service page

--- frontend/src/components/INDIServicePage.js
+++ frontend/src/components/INDIServicePage.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-const INDIServicePage = ({}) => (
-    <div />
-)
+const INDIServicePage = ({serverFound, serverRunning, onServerStopStartClick}) => {
+    if(! serverFound)
+        return null;
+    const stateParams = {
+        labelClass: serverRunning ? 'success' : 'danger',
+        label: serverRunning ? 'running' : 'not running',
+        buttonClass: serverRunning ? 'danger': 'success',
+        buttonLabel: serverRunning ? 'stop' : 'start',
+    }
 
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-xs-2">Status</div>
+                <div className="col-xs-2"><span className={'label label-' + stateParams.labelClass}>{stateParams.label}</span></div>
+                <div className="col-xs-2"><Button bsSize="xsmall" bsStyle={stateParams.buttonClass} onClick={onServerStopStartClick}>{stateParams.buttonLabel}</Button></div>
+            </div>
+        </div>
+    )
+}
 
