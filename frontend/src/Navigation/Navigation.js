c95acbc83a70bc946c17d5a690b566cd21a57b15
Marco Gulino <marco@gulinux.net>
2018-06-08 23:00:44 +0100

    Refactoring: export some utilities from Navigation

--- frontend/src/Navigation/Navigation.js
+++ frontend/src/Navigation/Navigation.js
@@ -5,25 +5,23 @@ import { Link } from 'react-router-dom';
 import { matchPath, Route} from 'react-router';
 
 
-const NavLinkItem = ({linkRef, label, match}) => { 
-    return (
-        <li className={!!match ? 'active' : ''}>
+export const NavLinkItem = ({linkRef, label, active, disabled}) => {
+    return disabled ? null : (
+        <li className={active ? 'active' : ''}>
             <Link to={linkRef}>
                 {label}
             </Link>
         </li>
-
-    )
-}
-
-const NavLink = ({linkRef, label, disabled}) => {
-    return disabled ? null : (
-        <Route path={linkRef}>
-            {({match}) => <NavLinkItem linkRef={linkRef} label={label}  match={match} />}
-        </Route>
     );
 }
 
+export const ActiveRoute = ({path, children, ...rest}) => {
+    return (
+        <Route path={path} {...rest}>
+            {({match}) => React.Children.map(children, child => React.cloneElement(child, {active: !!match})) }
+        </Route>
+    )
+}
 
 const Navigation = ({disabled, history}) => (
 <Navbar inverse collapseOnSelect>
@@ -35,11 +33,11 @@ const Navigation = ({disabled, history}) => (
   </Navbar.Header>
   <Navbar.Collapse>
     <Nav>
-        <NavLink linkRef="/sequences" label="Sequences" disabled={disabled} />
-        <NavLink linkRef="/indi" label="INDI Server" disabled={disabled} />
+        <ActiveRoute path="/sequences"><NavLinkItem linkRef="/sequences" label="Sequences" disabled={disabled} /></ActiveRoute>
+        <ActiveRoute path="/indi"><NavLinkItem linkRef="/indi" label="INDI Server" disabled={disabled} /></ActiveRoute>
     </Nav>
   </Navbar.Collapse>
-</Navbar> 
+</Navbar>
 )
 
 export default Navigation;
