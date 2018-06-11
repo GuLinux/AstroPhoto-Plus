aad9007586ac38aa19ac1eb147a6f1f33ec72d85
Marco Gulino <marco@gulinux.net>
2018-06-11 09:24:47 +0100

    semantic-ui for indi service devices page

--- frontend/src/INDI-Service/INDIServiceDevicesPage.js
+++ frontend/src/INDI-Service/INDIServiceDevicesPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Panel } from 'react-bootstrap';
+import { Accordion, Checkbox, Container, Label, Icon, Grid} from 'semantic-ui-react'
 
 const INDIDriver = ({driver, toggleDriverSelection, serverRunning }) => (
-    <div className="checkbox">
-        <label>
-            <input type="checkbox" onChange={(e) => toggleDriverSelection(driver.name, e.target.checked)} checked={driver.selected} disabled={serverRunning} /> {driver.label}
-        </label>
-    </div>
+    <Checkbox
+        label={driver.name}
+        onChange={(undefined, data) => toggleDriverSelection(driver.name, data.checked)}
+        checked={driver.selected}
+        disabled={serverRunning}
+        //toggle
+    />
 )
 
 class INDIDriversGroup extends React.Component {
@@ -15,34 +17,44 @@ class INDIDriversGroup extends React.Component {
         this.state = { expanded: this.hasSelectedDrivers() }
     }
 
-    toggle() {
-        this.setState({...this.state, expanded: ! this.state.expanded })
-    }
+    toggle = () => this.setState({...this.state, expanded: ! this.state.expanded })
 
-    hasSelectedDrivers() {
-        return this.props.group.selectedDrivers.length > 0;
-    }
+    hasSelectedDrivers = () => this.props.group.selectedDrivers.length > 0;
 
-    render() {
-        return (
-            <Panel expanded={this.state.expanded} onToggle={ () => this.toggle()} bsStyle={this.hasSelectedDrivers() ? 'primary' : 'default' }>
-                <Panel.Heading>
-                    <Panel.Title toggle className="driverGroupTitle">{this.props.group.name}</Panel.Title>
-                </Panel.Heading>
-                <Panel.Collapse>
-                    <Panel.Body>
-                        {
-                            this.props.group.drivers.map((d, index) => (
-                                <div key={index} className="col-xs-4">
-                                    <INDIDriver driver={this.props.drivers[d]} toggleDriverSelection={this.props.toggleDriverSelection} serverRunning={this.props.serverRunning} />
-                                </div>
-                            ))
-                        }
-                    </Panel.Body>
-                </Panel.Collapse>
-            </Panel>
-        )
-    }
+    titleIcon = () => this.hasSelectedDrivers() ? <Icon name='check'/> : null;
+
+    render = () => (
+        <Container>
+            <Accordion.Title
+                active={this.state.expanded}
+                onClick={ () => this.toggle()}
+            >
+                <Grid columns={12}>
+                    <Grid.Column width={11}>
+                        {this.props.group.name}
+                    </Grid.Column>
+                    <Grid.Column textAlign='right' floated='right'>
+                        {this.titleIcon()}
+                    </Grid.Column>
+                </Grid>
+            </Accordion.Title>
+            <Accordion.Content active={this.state.expanded}>
+                <Grid columns={2} stackable>
+                    {
+                        this.props.group.drivers.map((d, index) => (
+                            <Grid.Column>
+                                <INDIDriver
+                                    driver={this.props.drivers[d]}
+                                    toggleDriverSelection={this.props.toggleDriverSelection}
+                                    serverRunning={this.props.serverRunning}
+                                />
+                            </Grid.Column>
+                        ))
+                    }
+                </Grid>
+            </Accordion.Content>
+        </Container>
+    );
 }
 
 
@@ -50,9 +62,13 @@ const INDIServiceDevicesPage = ({groups, drivers, serverFound, serverRunning, to
     if(! serverFound)
         return null;
     return (
-        <div className="indi-service-page">
-            { Object.keys(groups).map( group => <INDIDriversGroup key={group} group={groups[group]} drivers={drivers} toggleDriverSelection={toggleDriverSelection} serverRunning={serverRunning} />)}
-        </div>
+        <Container>
+            <Accordion exclusive={false} fluid styled>
+                { Object.keys(groups).map( group =>
+                    <INDIDriversGroup key={group} group={groups[group]} drivers={drivers} toggleDriverSelection={toggleDriverSelection} serverRunning={serverRunning} />
+                )}
+            </Accordion>
+        </Container>
     )
 }
 
