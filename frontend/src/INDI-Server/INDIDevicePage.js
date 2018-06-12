327ae17ccb8441b98bef9aac61d51e259b7d229d
Marco Gulino <marco.gulino@gmail.com>
2018-06-12 22:45:40 +0100

    Redirect to indi page if device is not fount

--- frontend/src/INDI-Server/INDIDevicePage.js
+++ frontend/src/INDI-Server/INDIDevicePage.js
@@ -8,21 +8,25 @@ import { NavLink } from 'react-router-dom';
 
 
 
-const INDIDevicePage = ({device, groups, addPendingProperties, commitPendingProperties, messages}) => (
-    <Container>
-        <Menu secondary stackable>
-            <Menu.Item header content='Groups' />
-            { groups.map( group => <Menu.Item key={group} as={NavLink} to={`/indi/${device}/${group}`} content={group} /> )}
-        </Menu>
-        <Route path="/indi/:deviceId/:groupName" render={
-                ({match, location}) => <INDIDeviceGroupContainer location={location} device={match.params.deviceId} group={match.params.groupName} /> }
-        />
-        <Route path="/indi/:deviceId" exact={true} render={
-            ({match}) => <Redirect to={`/indi/${match.params.deviceId}/Main Control`} />
-        } />
-        <INDIMessagesPanel messages={messages} />
-        <INDIAutoloadConfiguration device={device} />
-    </Container>
-)
+const INDIDevicePage = ({device, groups, addPendingProperties, commitPendingProperties, messages}) => {
+    if(! device)
+        return <Redirect to='/indi' />
+    return (
+        <Container>
+            <Menu secondary stackable>
+                <Menu.Item header content='Groups' />
+                { groups.map( group => <Menu.Item key={group} as={NavLink} to={`/indi/${device}/${group}`} content={group} /> )}
+            </Menu>
+            <Route path="/indi/:deviceId/:groupName" render={
+                    ({match, location}) => <INDIDeviceGroupContainer location={location} device={match.params.deviceId} group={match.params.groupName} /> }
+            />
+            <Route path="/indi/:deviceId" exact={true} render={
+                ({match}) => <Redirect to={`/indi/${match.params.deviceId}/Main Control`} />
+            } />
+            <INDIMessagesPanel messages={messages} />
+            <INDIAutoloadConfiguration device={device} />
+        </Container>
+    )
+}
 
 export default INDIDevicePage
