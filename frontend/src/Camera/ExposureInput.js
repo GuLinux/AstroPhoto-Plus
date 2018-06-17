2314de1858e3288607344e645c84e9c41521f5a3
Marco Gulino <marco@gulinux.net>
2018-06-17 11:52:03 +0100

    Allow for subzero/decimal exposures

--- frontend/src/Camera/ExposureInput.js
+++ frontend/src/Camera/ExposureInput.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Input, Button } from 'semantic-ui-react';
 
-const parseExposure = (exposure) => parseInt(exposure, 10);
+const parseExposure = (exposure) => parseFloat(exposure);
 const exposureValid = (exposure) => parseExposure(exposure) > 0;
+const isNumber = (exposure) => !isNaN(parseExposure(exposure));
 
 const ExposureShootIcon = ({disabled, onClick}) => (
     <Button as='a' content='Shoot' icon='camera' disabled={disabled} size='tiny' onClick={onClick} />
@@ -13,9 +14,12 @@ const ExposureInput = ({shotParameters, onExposureChanged, onShoot, disabled}) =
         type='number'
         placeholder='seconds'
         size='tiny'
+        min={0}
+        max={999999}
+        step={0.1}
         onChange={(e, data) => onExposureChanged(parseExposure(data.value))}
         disabled={disabled}
-        value={exposureValid(shotParameters.exposure) ? parseExposure(shotParameters.exposure) : ''}
+        value={isNumber(shotParameters.exposure) ? parseExposure(shotParameters.exposure) : ''}
         label={
             <ExposureShootIcon disabled={disabled || ! exposureValid(shotParameters.exposure)} onClick={() => onShoot(shotParameters)} />
         }
