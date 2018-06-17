a8f7746db459c3b47d5c3e7851e87b3c2302a541
Marco Gulino <marco@gulinux.net>
2018-06-17 21:08:23 +0100

    More improvements/fixes to Camera page

--- frontend/src/Camera/ImageViewer.js
+++ frontend/src/Camera/ImageViewer.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image, Icon } from 'semantic-ui-react';
+
+class ImageComponent extends React.Component {
+    componentDidMount = () => this.props.onImageLoading && this.props.onImageLoading();
+
+    render = () => {
+        const { uri, onImageLoaded, fitScreen } = this.props;
+        let callbacks = onImageLoaded ? { onLoad: onImageLoaded, onError: onImageLoaded } : {};
+        return <Image alt='' ui={fitScreen} src={uri} fluid={!fitScreen} {...callbacks} />;
+    }
+}
 
 const ImageViewer = ({uri, fitScreen = false, onImageLoading, onImageLoaded}) => {
-    uri && onImageLoading && onImageLoading();
-    let callbacks = onImageLoaded ? { onLoad: onImageLoaded, onError: onImageLoaded } : {};
     return uri ? (
         <div className='image-viewer'>
-            { fitScreen ? <Image alt='' src={uri} {...callbacks} /> : <img alt='' src={uri} {...callbacks} /> }
+            <ImageComponent uri={uri} onImageLoaded={onImageLoaded} fitScreen={fitScreen} />
         </div>
-    ): null;
+    ): <Icon name='image outline' size='massive' disabled />;
 }
 export default ImageViewer;
