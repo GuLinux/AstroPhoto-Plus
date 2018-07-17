de8fa431866b7bc236d1c05964becc42d91fc2c8
Marco Gulino <marco@gulinux.net>
2018-07-17 23:25:04 +0100

    Safeguards for rendering images

--- frontend/src/Sequences/LastCapturedSequenceImage.js
+++ frontend/src/Sequences/LastCapturedSequenceImage.js
@@ -4,13 +4,26 @@ import { Link } from 'react-router-dom'
 import { ImageLoader } from '../Image/Image'
 
 
-const LastCapturedSequenceImage = ({type, lastImageId, lastImage}) => lastImage ? (
-    <Container>
-        <Header content='Last captured image' />
-        <Link to={`/image/main/${lastImageId}`}>
-            <ImageLoader id={lastImageId} type={type} uri={lastImage} fitScreen={true} />
-        </Link>
-    </Container>
-) : null;
+class LastCapturedSequenceImage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount = () => console.log('componentDidMount: ', this.props);
+    componentDidUpdate = () => console.log('componentDidUpdate: ', this.props);
+
+    render = () => {
+        const {type, lastImageId, lastImage} = this.props;
+        return lastImage ? (
+            <Container>
+                <Header content='Last captured image' />
+                <Link to={`/image/main/${lastImageId}`}>
+                    <ImageLoader key={lastImageId} id={lastImageId} type={type} uri={lastImage} fitScreen={true} />
+                </Link>
+            </Container>
+        ) : null;
+    }
+
+}
 
 export default LastCapturedSequenceImage;
