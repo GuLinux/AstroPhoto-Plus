a8ab87c68eca7794edb94129089515eb6747209d
Marco Gulino <marco.gulino@sky.uk>
2018-07-20 01:10:29 +0100

    Add reload button to error page

--- frontend/src/Errors/ErrorPage.js
+++ frontend/src/Errors/ErrorPage.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Message } from 'semantic-ui-react'
+import { Message, Button } from 'semantic-ui-react'
+import { withRouter } from 'react-router';
 
-const ErrorPage = ({errorSource, errorPayload}) => (
+const ErrorPage = withRouter( ({history, location, errorSource, errorPayload}) => (
     <Message error>
         <Message.Header>Server error</Message.Header>
         <p>It looks like your backend server is not running, or encountered a fatal error.</p>
         <p>Please check your backend server is up and running, and try reloading this page.</p>
+        <Button onClick={() => window.location.reload()} content='Reload' icon='sync'/>
         <h3>Error details:</h3>
         <p>Error source: {errorSource}</p>
         <pre>{errorPayload}</pre>
     </Message>
+)
 )
 
 export default ErrorPage;
