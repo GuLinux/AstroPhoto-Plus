f4448946afa8f74dded16ee6e7ace15ff3638cc8
Marco Gulino <marco.gulino@sky.uk>
2018-07-31 19:30:09 +0100

    Allow backend server to serve frontend static files in production mode

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 import json
+import os
+
+static_folder = os.path.abspath(os.environ.get('STARQUEW_STATIC_FOLDER', '../frontend'))
+has_static_folder = os.path.isfile(os.path.join(static_folder, 'index.html'))
 
 
-app = Flask('StarQuew')
+
+app = Flask('StarQuew', static_folder=None)
+app.config['has_static_folder'] = has_static_folder
+app.config['static_folder'] = static_folder
+
 with open('version.json', 'r') as version_json:
     app.config['version'] = json.load(version_json)
 
