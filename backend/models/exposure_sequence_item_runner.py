f986c653dae7f1665a7db4677de0b56ad99e0621
Marco Gulino <marco.gulino@sky.uk>
2018-08-19 19:49:11 +0100

    Save files directly from memory buffer

--- backend/models/exposure_sequence_item_runner.py
+++ backend/models/exposure_sequence_item_runner.py
@@ -47,14 +47,8 @@ class ExposureSequenceItemRunner:
         self.callbacks.run('on_each_saved', self, item.sequence, item.file_name)
 
     def run(self):
-        self.camera.set_upload_to('both')
-        tmp_prefix = '__sequence_TMP'
-        tmp_upload_path = tempfile.gettempdir()
-        tmp_file = os.path.join(tmp_upload_path, tmp_prefix + '.fits')
+        self.camera.set_upload_to('client')
 
-        save_async_fits = SaveAsyncFITS(on_saved=self.on_saved)
-
-        self.camera.set_upload_path(tmp_upload_path, tmp_prefix)
         self.callbacks.run('on_started', self)
 
         shots_queue = Queue()
@@ -65,60 +59,22 @@ class ExposureSequenceItemRunner:
             for sequence in range(self.finished, self.count):
                 if self.stopped:
                     self.callbacks.run('on_stopped', self, sequence)
-                    save_async_fits.join()
+                    shots_queue.put({'type': 'stopped'})
                     return
 
                 self.callbacks.run('on_each_started', self, sequence)
-                temp_before = self.ccd_temperature
-                time_started = time.time()
                 shots_queue.put({
                     'type': 'shot',
                     'index': sequence,
-                    'initial_temperature': temp_before,
                 })
                 self.camera.shoot(self.exposure)
-                temp_after = self.ccd_temperature
-                time_finished = time.time()
-                
-                format_params = { 'exposure': self.exposure, 'number': sequence + self.start_index}
-                format_params.update(self.filename_template_params)
-
-                for key, value in format_params.items():
-                    if callable(value):
-                        format_params[key] = value(self)
-
-                file_name = os.path.join(self.upload_path, self.filename_template.format(**format_params))
-
-                file_suffix_to_skip_overwriting = 1
-                output_file = file_name
-                while os.path.isfile(output_file):
-                    filepath, extension = os.path.splitext(file_name)
-                    output_file = '{}__{}{}'.format(filepath, file_suffix_to_skip_overwriting, extension)
-                    file_suffix_to_skip_overwriting += 1
-
-
-
-                unique_temp_file = os.path.join(tmp_upload_path, '__seq_tmp_{}'.format(time_finished))
-                os.rename(tmp_file, unique_temp_file)
 
                 self.finished += 1
-                self.callbacks.run('on_each_finished', self, sequence, output_file)
-
-                save_async_fits.put({
-                    'type': 'exposure_finished',
-                    'number': sequence+1,
-                    'exposure': self.exposure,
-                    'time_started': time_started,
-                    'time_finished': time_finished,
-                    'temperature_started': temp_before,
-                    'temperature_finished': temp_after,
-                    'temp_filename': unique_temp_file,
-                    'output_file': output_file,
-                })
+                self.callbacks.run('on_each_finished', self, sequence, self.__output_file(sequence))
 
         finally:
             shots_queue.put({'type': 'finished'})
-            save_async_fits.finished()
+
 
 
         # Note: this means we wait for *all* images in this sequences to be saved, before starting the next sequence. Which might be a bottleneck, but then again, if you're shooting much faster than you can save, then you probably have a bigger issue...
@@ -127,12 +83,12 @@ class ExposureSequenceItemRunner:
 
     def __blob_watcher(self, shots_queue):
         finished = False
+        save_async_fits = SaveAsyncFITS(on_saved=self.on_saved)
         indi_host, indi_port = self.server.client.host, self.server.client.port
-        logger.debug('**** starting secondary indi client: host={}, port={}'.format(indi_host, indi_port))
+
         blobs_queue = Queue()
         def on_new_blob(bp):
             blob_info = 'name={}, label={}, format={}, bloblen={}, size={}'.format(bp.name, bp.label, bp.format, bp.bloblen, bp.size)
-            logger.debug('******** got new blob! {}'.format(blob_info))
             blobs_queue.put(bp)
 
         indi_client = INDIClient(address=indi_host, port=indi_port, callbacks={
@@ -152,15 +108,50 @@ class ExposureSequenceItemRunner:
             item = shots_queue.get()
             if item['type'] == 'finished':
                 finished = True
+                save_async_fits.finished()
+            elif item['type'] == 'stopped':
+                save_async_fits.join()
             elif item['type'] == 'shot':
                 time_started = time.time()
+                temp_before = self.ccd_temperature
                 blob = blobs_queue.get()
                 temp_after = self.ccd_temperature
                 time_finished = time.time()
-                logger.debug('**** now saving blob')
+
+                save_async_fits.put({
+                    'type': 'exposure_finished',
+                    'number': item['index'] + self.start_index,
+                    'exposure': self.exposure,
+                    'time_started': time_started,
+                    'time_finished': time_finished,
+                    'temperature_started': temp_before,
+                    'temperature_finished': temp_after,
+                    'output_file': self.__output_file(item['index']),
+                    'blob': blob.getblobdata(),
+                })
+
+
+
         indi_client.disconnectServer()
 
 
+    def __output_file(self, sequence):
+        format_params = { 'exposure': self.exposure, 'number': sequence + self.start_index}
+        format_params.update(self.filename_template_params)
+
+        for key, value in format_params.items():
+            if callable(value):
+                format_params[key] = value(self)
+
+        file_name = os.path.join(self.upload_path, self.filename_template.format(**format_params))
+
+        file_suffix_to_skip_overwriting = 1
+        output_file = file_name
+        while os.path.isfile(output_file):
+            filepath, extension = os.path.splitext(file_name)
+            output_file = '{}__{}{}'.format(filepath, file_suffix_to_skip_overwriting, extension)
+            file_suffix_to_skip_overwriting += 1
+        return output_file
 
 
     @property
