a7a1510cc3903d06daa879054a4308aab32b3491
Marco Gulino <marco@gulinux.net>
2018-12-09 12:38:33 +0000

    Fix sequence list page

--- frontend/src/Sequences/SequencesListContainer.js
+++ frontend/src/Sequences/SequencesListContainer.js
@@ -1,25 +1,9 @@
 import { connect } from 'react-redux';
 import { SequencesList } from './SequencesList';
-import Actions from '../actions';
-import { getSequencesGears } from '../Gear/selectors';
-import { getSequences } from './selectors';
-
-const mapStateToProps = state => ({
-    sequences: getSequences(state),
-    gear: getSequencesGears(state),
-})
-
-const mapDispatchToProps = {
-    onSequenceDelete: Actions.Sequences.remove,
-    startSequence: Actions.Sequences.start,
-    duplicateSequence: Actions.Sequences.duplicate,
-    stopSequence: Actions.Sequences.stop,
-    resetSequence: Actions.Sequences.reset,
-}
+import { sequenceListSelector } from './selectors';
 
 const SequencesListContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
+  sequenceListSelector,
 )(SequencesList)
 
 export default SequencesListContainer
--- frontend/src/Sequences/selectors.js
+++ frontend/src/Sequences/selectors.js
@@ -1,13 +1,15 @@
 import { createSelector } from 'reselect';
 import { list2object, filterChildren } from '../utils'; 
+import { getDevices } from '../INDI-Server/selectors-redo';
 
-
-export const getSequenceIds = state => state.sequences.ids;
+const getSequenceIds = state => state.sequences.ids;
 
 const sequenceGearConnected = (gear) => gear &&  
                                 (!gear.camera || gear.camera.connected) &&
                                 (!gear.filterWheel || gear.filterWheel.connected);
 
+
+const getSequenceJobs = state => state.sequenceJobs;
 
 
 export const getSequenceEntities = createSelector([state => state.sequences.entities], sequences => {
@@ -27,7 +29,7 @@ export const getSequenceEntities = createSelector([state => state.sequences.enti
     });
     return entities;
 });
-export const getSequenceJobs = state => state.sequenceJobs;
+
 
 export const getSequences = createSelector([getSequenceIds, getSequenceEntities], (ids, entities) => {
   return ids.map(id => entities[id])
@@ -36,3 +38,39 @@ export const getSequences = createSelector([getSequenceIds, getSequenceEntities]
 export const getSequenceEntitiesWithJobs = createSelector([getSequences, getSequenceJobs], (sequences, jobs) =>
     list2object(sequences.map(s => ({...s, sequenceJobEntities: filterChildren(jobs, i => i.sequence === s.id) }) ), 'id')
 );
+
+
+
+export const sequenceListSelector = createSelector([getSequenceIds], (sequences) => ({
+    sequences,
+}));
+
+
+const getSequence = (sequenceId) => state => state.sequences.entities[sequenceId];
+
+const getSequenceGear = (sequenceId) => createSelector([getSequence(sequenceId), getDevices],
+    (sequence, devices) => ({
+        camera: devices.entities[sequence.camera],
+        filterWheel: sequence.filterWheel && devices.entities[sequence.filterWheel],
+})); 
+
+export const getSequenceListItemSelector = (sequenceId) => createSelector([
+    getSequence(sequenceId),
+    getSequenceGear(sequenceId),
+    state => getSequenceJobs(state).length
+], (sequence, gear, sequenceJobsLength) => {
+    const gearConnected = gear.camera && gear.camera.connected && (!sequence.filterWheel || (gear.filterWheel && gear.filterWheel.connected));
+    const canStop = sequence.status === 'running';
+    const canEdit = gearConnected && ['idle', 'error'].includes(sequence.status);
+    const canReset = sequence.status === 'running';
+    return {
+        sequence,
+        gear,
+        sequenceJobsLength,
+        canStart: gearConnected,
+        canStop,
+        canEdit,
+        canReset,
+    }
+});
+
