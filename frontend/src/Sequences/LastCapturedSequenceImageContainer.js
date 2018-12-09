e0204235abd14f6202baf17117248f64f1c2c138
Marco Gulino <marco@gulinux.net>
2018-12-09 13:54:11 +0000

    Minor refactoring

--- frontend/src/Sequences/LastCapturedSequenceImageContainer.js
+++ frontend/src/Sequences/LastCapturedSequenceImageContainer.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import LastCapturedSequenceImage from './LastCapturedSequenceImage';
+import { LastCapturedSequenceImage } from './LastCapturedSequenceImage';
 import { lastCapturedSequenceImageSelector } from './selectors';
 import { imageUrlBuilder } from '../utils';
 import Actions from '../actions';
