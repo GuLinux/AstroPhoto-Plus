b18bf8e73d632f70a2d3c6f444ef3c1c95c0e99b
Marco Gulino <marco@gulinux.net>
2019-01-20 16:16:54 +0000

    Add transition animation between pages

--- frontend/src/Navigation/HistoryLandingPage.js
+++ frontend/src/Navigation/HistoryLandingPage.js
@@ -1,4 +1,4 @@
-import { Route, Switch, Redirect } from 'react-router';
+import { Route, Switch, Redirect, withRouter } from 'react-router';
 import React from 'react';
 
 class HistoryLandingRoutes extends React.PureComponent {
@@ -11,24 +11,35 @@ class HistoryLandingRoutes extends React.PureComponent {
 
     componentDidMount = () => {
         this.setLandingPath();
+        console.log('M', this.props.route, this.props.landingPath, this.props.currentPath)
     }
 
     componentDidUpdate = (prevProps) => {
+        console.log('U', this.props.route, this.props.landingPath, this.props.currentPath)
         if(this.props.currentPath !== prevProps.currentPath) {
             this.setLandingPath();
         }
     }
 
+    renderRedirect = () => <Redirect to={this.props.landingPath} />;
+
     render = () => (
         <Switch>
-            <Route path={this.props.route} exact={true} render={() => <Redirect to={this.props.landingPath} />} />
+            <Route path={this.props.route} exact={true} render={this.renderRedirect} />
             {this.props.children}
         </Switch>
 
     )
 }
 
-export const HistoryLandingPage = ({route, landingPath, children, setLandingPath}) => (
-    <Route path={route} render={ ({location}) => <HistoryLandingRoutes route={route} currentPath={location.pathname} landingPath={landingPath} setLandingPath={setLandingPath} children={children} /> } />
-)
-
+export const HistoryLandingPage = withRouter(
+    ({location, route, landingPath, children, setLandingPath}) => 
+    <HistoryLandingRoutes
+        location={location}
+        route={route}
+        currentPath={location.pathname}
+        landingPath={landingPath}
+        setLandingPath={setLandingPath}
+        children={children} 
+    /> 
+); 
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -10,22 +10,30 @@ import { SettingsContainer } from '../Settings/SettingsContainer';
 import { ImagePage } from '../Image/ImagePage';
 import { PlateSolvingPageContainer } from '../PlateSolving/PlateSolvingPageContainer';
 import './App.css';
-import { Route, Redirect } from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 import { Routes } from '../routes';
 import { Home } from './Home';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 
 const App = ({location}) => (
   <div className="App">
     <Navbar location={location}>
         <NotificationsContainer />
         <ErrorPageContainer>
-            <Route exact path={Routes.ROOT} component={Home}/>
-            <Route path={Routes.SEQUENCES_PAGE} component={SequencesPage} />
-            <Route path={Routes.INDI_PAGE} component={INDIServerContainer} />
-            <Route path={Routes.CAMERA_PAGE} component={CameraContainer} />
-            <Route path={Routes.PLATE_SOLVING_PAGE} component={PlateSolvingPageContainer} />
-            <Route path={Routes.SETTINGS_PAGE} component={SettingsContainer} />
-            <Route path={Routes.IMAGE_PAGE} render={({match, location}) => <ImagePage id={match.params.id} type={match.params.type} />} />
+          <TransitionGroup>
+            <CSSTransition key={location.key} classNames='fade' timeout={500}>
+              <Switch location={location}>
+                <Route exact path={Routes.ROOT} component={Home}/>
+                <Route path={Routes.SEQUENCES_PAGE} component={SequencesPage} />
+                <Route path={Routes.INDI_PAGE} component={INDIServerContainer} />
+                <Route path={Routes.CAMERA_PAGE} component={CameraContainer} />
+                <Route path={Routes.PLATE_SOLVING_PAGE} component={PlateSolvingPageContainer} />
+                <Route path={Routes.SETTINGS_PAGE} component={SettingsContainer} />
+                <Route path={Routes.IMAGE_PAGE} render={({match, location}) => <ImagePage id={match.params.id} type={match.params.type} />} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         </ErrorPageContainer>
         <LoadingContainer />
     </Navbar>
