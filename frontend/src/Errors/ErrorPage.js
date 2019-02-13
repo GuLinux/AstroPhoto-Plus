3d07c7e29438ee15541a2802d367090a91404516
Marco Gulino <marco.gulino@sky.uk>
2019-02-13 19:37:50 +0000

    Wrap ErrorPage into a container

--- frontend/src/Errors/ErrorPage.js
+++ frontend/src/Errors/ErrorPage.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Message, Button } from 'semantic-ui-react'
+import { Container, Image, Message, Button } from 'semantic-ui-react'
 import { withRouter } from 'react-router';
 
 const ErrorPage = withRouter( ({history, location, errorSource, errorPayload}) => (
-    <Message error>
-        <Message.Header>Server error</Message.Header>
-        <p>It looks like your backend server is not running, or encountered a fatal error.</p>
-        <p>Please check your backend server is up and running, and try reloading this page.</p>
-        <Button onClick={() => window.location.reload()} content='Reload' icon='sync'/>
-        <h3>Error details:</h3>
-        <p>Error source: {errorSource}</p>
-        <pre>{errorPayload}</pre>
-    </Message>
-)
-)
+    <Container>
+        <Image src='/images/site-logo-dark-bg.svg'/>
+        <Message error>
+            <Message.Header>Server error</Message.Header>
+            <p>It looks like your backend server is not running, or encountered a fatal error.</p>
+            <p>Please check your backend server is up and running, and try reloading this page.</p>
+            <Button onClick={() => window.location.reload()} content='Reload' icon='sync'/>
+            <h3>Error details:</h3>
+            <p>Error source: {errorSource}</p>
+            <pre>{errorPayload}</pre>
+        </Message>
+    </Container>
+));
 
 export default ErrorPage;
