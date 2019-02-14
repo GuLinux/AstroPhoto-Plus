7ceefa1559dad1537c9989de66ad6ce68c11dcf6
Marco Gulino <marco.gulino@sky.uk>
2019-02-14 19:37:24 +0000

    Merge

--- frontend/src/PlateSolving/PlateSolving.js
+++ frontend/src/PlateSolving/PlateSolving.js
@@ -21,7 +21,7 @@ class SetCameraFOV extends React.PureComponent {
         const { setOption, telescopeFocalLength, ccdInfo } = this.props;
         const sensorWidth = getSensorSizeFromResolution(ccdInfo.ccdMaxX, ccdInfo.ccdPixelSizeX);
         const fieldOfView = getFieldOfView(telescopeFocalLength, sensorWidth);
-        const fovRange = { minimumWidth: fieldOfView * 0.9, maximumWidth: fieldOfView * 1.1 };
+        const fovRange = { minimumWidth: fieldOfView * 0.95, maximumWidth: fieldOfView * 1.05 };
         setOption(Options.fov, fovRange);
     }
     render = () => null;
@@ -209,8 +209,8 @@ export class PlateSolving extends React.PureComponent {
                             /> }
                             { options[Options.fovSource] ? (
                                 <Grid.Row>
-                                <Grid.Column width={8}><NumericInput min={0} label='Minimum width (arcminutes)' size='mini' readOnly={!isManualFOV || loading } disabled={!isManualFOV || loading } value={get(options, [Options.fov, 'minimumWidth'], 0)} onChange={this.setMinimumFOV}/></Grid.Column>
-                                <Grid.Column width={8}><NumericInput min={get(options, [Options.fov, 'maximumWidth'], 0)} label='Maximum width (arcminutes)' size='mini' readOnly={!isManualFOV || loading } disabled={!isManualFOV || loading } value={get(options, [Options.fov, 'maximumWidth'], 0)} onChange={this.setMaximumFOV}/></Grid.Column>
+                                <Grid.Column width={8}><NumericInput min={0} max={get(options, [Options.fov, 'maximumWidth'], 0)} label='Minimum width (arcminutes)' size='mini' readOnly={!isManualFOV || loading } disabled={!isManualFOV || loading } value={get(options, [Options.fov, 'minimumWidth'], 0)} onChange={this.setMinimumFOV}/></Grid.Column>
+                                <Grid.Column width={8}><NumericInput min={get(options, [Options.fov, 'minimumWidth'], 0)} label='Maximum width (arcminutes)' size='mini' readOnly={!isManualFOV || loading } disabled={!isManualFOV || loading } value={get(options, [Options.fov, 'maximumWidth'], 0)} onChange={this.setMaximumFOV}/></Grid.Column>
                             </Grid.Row>
                             ) : (
                                 <Grid.Row>
