70c0b772c1d5eb25e706652d92ae35e66037b042
Marco Gulino <marco@gulinux.net>
2019-09-28 18:03:08 +0100

    Added mp ProcessWorker class decorator

---
+++ backend/utils/test_processworker.py
@@ -0,0 +1,22 @@
+import mp
+
+@mp.ProcessWorker
+class Foo:
+    def __init__(self, tag):
+        self.tag = tag
+
+    def print_foo(self):
+        print('[{}]: foo'.format(self.tag))
+
+    def print_args(self, arg1, arg2='<none>'):
+        print('[{}]: {}, {}'.format(self.tag, arg1, arg2))
+
+    def raise_error(self):
+        raise RuntimeError('hello world')
+
+    def get_tag(self):
+        return self.tag
+
+foo = Foo('Foo-Tag')
+foo.start()
+
