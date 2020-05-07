e382828b6cecf52b3c186d6ce3a4bcc5988f3f36
Marco Gulino <marco@gulinux.net>
2020-05-07 22:00:09 +0100

    Rewrite CSS theme using Semantic Ui "flat" theme as a base, with dark colours (#192)

--- frontend/scripts/remove_googlefonts_call.py
+++ frontend/scripts/remove_googlefonts_call.py
@@ -4,11 +4,21 @@ import re
 
 
 project_dir = (os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-css_file_path = os.path.join(project_dir, 'public', 'themes', 'bootswatch-v3', 'semantic.slate.min.css')
+css_file_path = os.path.join(project_dir, 'node_modules/forest-themes/dist/semantic-ui/v2/semantic.flat.min.css')
 
-gfont_re = re.compile('@import url\(https:\/\/fonts\.googleapis\.com.*\);$')
+#gfont_re = re.compile('@import url\(https:\/\/fonts\.googleapis\.com.*\);$')
+gfont_re = re.compile('@import url\(https:\/\/fonts\.googleapis\.com.*\);')
+
+def remove_gfonts(line):
+    new_line = gfont_re.sub('', line)
+    if new_line != line:
+        print('*** fonts url removed:')
+        print('<<<{}'.format(line))
+        print('>>>{}'.format(new_line))
+    return new_line
+
 with open(css_file_path, 'r') as f:
-    lines = [gfont_re.sub('', line) for line in f.readlines()]
+    lines = [remove_gfonts(line) for line in f.readlines()]
 
 with open(css_file_path, 'w') as f:
     for line in lines:
