660dd856a91cfa3015b7768767a4ba5733671fb3
Marco Gulino <marco@gulinux.net>
2020-05-19 20:48:03 +0100

    * Implement retry for EFW jobs *
    As EFW are relatively prone to fail, a retry mechanism is now implemented to try and avoid sequences failing.
    Filter wheel jobs now retry changing the filter up to 5 times, each time with incrementing time between attempts.
    
    fixes #215

--- backend/sequences/filter_wheel_sequence_job.py
+++ backend/sequences/filter_wheel_sequence_job.py
@@ -1,4 +1,5 @@
 from app import logger
+import time
 
 
 class FilterWheelSequenceJob:
@@ -12,11 +13,26 @@ class FilterWheelSequenceJob:
 
     def run(self, server, devices, root_path, event_listener, on_update, index):
         wheel = devices['filter_wheel']
+        retry = 0
+        while True:
+            try:
+                self.__change_filter(wheel)
+                return
+            except RuntimeError as e:
+                if retry == 5:
+                    raise e
+                retry += 1
+                logger.error('{} - retrying ({} of 5)'.format(e, retry))
+                time.sleep(2 * retry)
+
+
+    def __change_filter(self, wheel):
         logger.info('Changing filter to {} on filter wheel {}'.format(self.filter_number, wheel))
         wheel.indi_sequence_filter_wheel().set_filter_number(self.filter_number)
         
         if wheel.indi_sequence_filter_wheel().current_filter()[0] != self.filter_number:
             raise RuntimeError('Error changing filter wheel to filter {}'.format(self.filter_number))
+
 
 
     def reset(self, remove_files=False):
